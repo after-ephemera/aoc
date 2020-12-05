9e1961868b2a53fb1710c2205208c669112fb33f
Jk Jensen <azjkjensen@gmail.com>
2020-12-04 22:18:29 -0800

    consolidate into days

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,8 +1,41 @@
 use std::fs::File;
 use std::io::Read;
 
-fn main() {
+fn part_2() {
     let mut f = File::open("src/day1/input.txt").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+    for sub in s.split('\n') {
+        for sub2 in s.split('\n') {
+            for sub3 in s.split('\n') {
+                if sub == "" || sub2 == "" || sub3 == "" {
+                    continue;
+                }
+                //println!("{}", sub);
+                //println!("{}", sub2);
+                if sub.parse::<i32>().unwrap()
+                    + sub2.parse::<i32>().unwrap()
+                    + sub3.parse::<i32>().unwrap()
+                    == 2020
+                {
+                    println!(
+                        "found it! {} * {} * {} = {}",
+                        sub,
+                        sub2,
+                        sub3,
+                        sub.parse::<i32>().unwrap()
+                            * sub2.parse::<i32>().unwrap()
+                            * sub3.parse::<i32>().unwrap()
+                    );
+                    return;
+                }
+            }
+        }
+    }
+}
+
+fn part_1() {
+    let mut f = File::open("src/day1/input2.txt").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
     for sub in s.split('\n') {
@@ -23,4 +56,9 @@ fn main() {
             }
         }
     }
+}
+
+fn main() {
+    part_1();
+    part_2();
 }
--- src/day3/main.rs
+++ src/day3/main.rs
@@ -27,9 +27,9 @@ impl Map {
         }
     }
 
-    fn slide(&mut self) {
-        self.toboggan.row += 1;
-        self.toboggan.col += 3;
+    fn slide(&mut self, hz: usize, vrt: usize) {
+        self.toboggan.row += vrt;
+        self.toboggan.col += hz;
         self.toboggan.col = self.toboggan.col % self.width;
 
         if self.toboggan.row > self.height - 1 {
@@ -51,7 +51,7 @@ fn gen_map(map_str: &str) -> Map {
         if line == "" {
             continue;
         }
-        println!("line:{}", line);
+        //println!("line:{}", line);
         let mut sub_v: Vec<char> = vec![];
         for c in line.chars() {
             sub_v.push(c);
@@ -60,20 +60,33 @@ fn gen_map(map_str: &str) -> Map {
         locations.push(sub_v);
         width = line.chars().count();
     }
-    //println!("{:?} x {:?}", locations.len(), locations[0].len());
-    //println!("{:?}", locations[0]);
-    //println!("{:?}", locations[322]);
     Map::new(width, height, locations)
+}
+
+fn trees_hit(mut map: Map, horizontal_slide: usize, vertical_slide: usize) -> u32 {
+    while map.toboggan.row < map.height - 1 {
+        map.slide(horizontal_slide, vertical_slide);
+    }
+    println!(
+        "found {} trees for {}, {}",
+        map.trees_found, horizontal_slide, vertical_slide
+    );
+    map.trees_found
 }
 
 fn main() -> Result<()> {
     let map_str = read_to_string("src/day3/input.txt")?;
-    let mut map = gen_map(&map_str);
-
-    while map.toboggan.row < map.height - 1 {
-        map.slide();
-    }
-    println!("found {} trees", map.trees_found);
+    let map = gen_map(&map_str);
+    let t11 = trees_hit(map, 1, 1);
+    let map = gen_map(&map_str);
+    let t31 = trees_hit(map, 3, 1);
+    let map = gen_map(&map_str);
+    let t51 = trees_hit(map, 5, 1);
+    let map = gen_map(&map_str);
+    let t71 = trees_hit(map, 7, 1);
+    let map = gen_map(&map_str);
+    let t12 = trees_hit(map, 1, 2);
+    println!("final result {}", t11 * t31 * t51 * t71 * t12);
 
     Ok(())
 }
